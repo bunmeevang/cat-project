57d64eb234a3cea7bd7f5eea395345531f99a744
bunmeev <bunmee.vang@gmail.com>
2021-04-12 09:33:12 -0400

    more css

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
             }}
             />
         </Switch>
-        <Footer />
+        {/* <Footer /> */}
       </Router>
     </div>
   );
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import './Footer.css';
+// import React from 'react';
+// import './Footer.css';
 
-export default function Footer(){
-    return(
-        <div className="footer-main">
-            <p>CatApp by Bunmee Vang</p>
-        </div>
-    ) 
-}
+// export default function Footer(){
+//     return(
+//         <div className="footer-main">
+//             <p>CatApp by Bunmee Vang</p>
+//         </div>
+//     ) 
+// }
